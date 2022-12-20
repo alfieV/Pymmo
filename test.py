e1c74f5b30a322b2f660d5d2d9e9517ad588da9d
mathi <mathi@SPECTRE_MATHIS>
2022-12-20 18:15:56 +0100

    ajout de séries

--- test.py
+++ test.py
@@ -7,9 +7,18 @@ df = pd.read_csv('DATA/valeursfoncieres-2018.txt', sep='|', low_memory=False)
 #print (df, '\n')
 print (df.describe(), '\n')
 
+#Les 7 premières valeurs sont confidentiels (identité de la personne etc...)
 numDisposition = df['No disposition']
 dateMutation = df['Date mutation']
 natureMutation = df['Nature mutation']
 valeurFonciere = df['Valeur fonciere']
-
+numVoie = df['No voie']
+indiceRepetition = df['B/T/Q']
+typeVoie = df['Type de voie']
+codeRivoli = df['Code voie']
+nomVoie = df['Voie']
+codepostal = df['Code postal']
+nomCommune = df['Commune']
+codeDepartement = df['Code departement']
+codeCommune = df['Code commune']
 print (valeurFonciere, '\n')
