7a9bd3e9931d4d59af8ee14cf631ad995cad7b6e
mathi <mathi@SPECTRE_MATHIS>
2022-12-20 17:55:43 +0100

    start

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #Dataset
-df = pd.read_csv('DATA/valeursfoncieres-2018.txt')
-print (type(df), '\n')
+df = pd.read_csv('DATA/valeursfoncieres-2018.txt', sep='|', low_memory=False)
+#print (type(df), '\n')
 
 print (df, '\n')
