4cdb531a1cb3f566417c41f670d3bad1c76ea363
mathi <mathi@SPECTRE_MATHIS>
2022-12-20 18:05:15 +0100

    debut affectation series

--- test.py
+++ test.py
@@ -4,4 +4,12 @@ import pandas as pd
 df = pd.read_csv('DATA/valeursfoncieres-2018.txt', sep='|', low_memory=False)
 #print (type(df), '\n')
 
-print (df, '\n')
+#print (df, '\n')
+print (df.describe(), '\n')
+
+numDisposition = df['No disposition']
+dateMutation = df['Date mutation']
+natureMutation = df['Nature mutation']
+valeurFonciere = df['Valeur fonciere']
+
+print (valeurFonciere, '\n')
