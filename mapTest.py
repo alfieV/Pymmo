d2868d76bed2b4520a44c45b234d54df2d13f054
mathi <mathi@SPECTRE_MATHIS>
2022-12-29 18:32:55 +0100

    maj map

--- mapTest.py
+++ mapTest.py
@@ -4,7 +4,7 @@ import requests, json
 import urllib.parse
 
 api_url = "https://api-adresse.data.gouv.fr/search/?q="
-adr = "53, rue Etienne Marcel, 94210 Saint Maur des Fossés"
+adr = "2, boulevard Blaise Pascal, 93162 Noisy le Grand"
 r = requests.get(api_url + urllib.parse.quote(adr))
 
 dict_coords = json.loads(r.content.decode('unicode_escape'))
@@ -12,6 +12,6 @@ dict_coords = json.loads(r.content.decode('unicode_escape'))
 coords = (dict_coords['features'][0]['geometry']['coordinates'][1], dict_coords['features'][0]['geometry']['coordinates'][0])
 
 map = folium.Map(location=coords, tiles='OpenStreetMap', zoom_start=15)
-folium.Marker(location = coords, popup="Maison des Pipart").add_to(map)
+folium.Marker(location = coords, popup="ESIEE").add_to(map)
 
 map.save(outfile='map2.html')
